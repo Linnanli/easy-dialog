4d54a39b376ce15c55d611328e45789304c8e23a
Lin <845058952@qq.com>
2018-02-19 21:24:22 +0800

    压缩js脚本

--- build/webpack.component.build.js
+++ build/webpack.component.build.js
@@ -4,31 +4,32 @@ var merge = require('webpack-merge');
 var HTMLWebpackPlugin = require('html-webpack-plugin');
 var ExtrctTextPlugin = require('extract-text-webpack-plugin');
 var cleanWebpackPlugin = require('clean-webpack-plugin');
+var ParallelUglifyPlugin = require('webpack-parallel-uglify-plugin');
 
 var common = require('./webpack.common');
 var styleConifg = require('./style.config');
 
-function resolve(pathstr){
-    return path.resolve(__dirname,pathstr);
+function resolve(pathstr) {
+    return path.resolve(__dirname, pathstr);
 }
 
-module.exports = merge(common,{
-    entry:{
-        'simple-dialog':resolve('../src/index.js'),
-        '/lib/dialog/index':resolve('../src/component/dialog/index.js')
+module.exports = merge(common, {
+    entry: {
+        'simple-dialog': resolve('../src/index.js'),
+        '/lib/dialog/index': resolve('../src/component/dialog/index.js')
     },
-    output:{
+    output: {
         path: resolve('../dist'),
-        filename:'[name].js',
-        library:'simple-dialog',
-        libraryTarget:'umd'
+        filename: '[name].js',
+        library: 'simpleDialog',
+        libraryTarget: 'umd'
     },
-    module:{
+    module: {
         rules: styleConifg(true).styleLoader
     },
-    plugins:[
+    plugins: [
         new webpack.DefinePlugin({
-            'process.env':{
+            'process.env': {
                 NODE_ENV: JSON.stringify('production')
             }
         }),
@@ -36,6 +37,24 @@ module.exports = merge(common,{
         new cleanWebpackPlugin(['dist']),
         new ExtrctTextPlugin({
             filename: 'simple-dialog.css'
+        }),
+        new ParallelUglifyPlugin({
+            cacheDir: '.uglify-cache',
+            sourceMap: true,
+            output: {
+                beautify: true,
+                comments: true
+            },
+            compress: {
+                // 在UglifyJs删除没有用到的代码时不输出警告
+                warnings: false,
+                // 删除所有的 `console` 语句，可以兼容ie浏览器
+                drop_console: true,
+                // 内嵌定义了但是只用到一次的变量
+                collapse_vars: true,
+                // 提取出出现多次但是没有定义成变量去引用的静态值
+                reduce_vars: true
+            }
         })
     ]
 });
--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@
  * @Author: linnanli 
  * @Date: 2018-02-19 00:28:39 
  * @Last Modified by: linnanli
- * @Last Modified time: 2018-02-19 17:50:20
+ * @Last Modified time: 2018-02-19 20:57:30
  * @Dscription: dialog 入口文件 
 */
 // require('./styles/index.scss');
-require('./component/dialog');
+var dialog = require('./component/dialog');
+
+module.exports = {
+    dialog: dialog
+};
